4b8e426a21bec2aae4e27791390090d7656d65a2
nikvicros <nikho.rosales@gmail.com>
2017-04-12 16:44:22 -0500

    Added more structure to People Comp

--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Popup } from 'ng2-opd-popup';
 
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
   styles: []
 })
-export class PeopleComponent implements OnInit {
 
-  constructor() { }
+export class PeopleComponent {
 
-  ngOnInit() {
+  @ViewChild('popup1') popup1: Popup;
+  clickButton(){ 
+    this.popup1.options = {
+    header: "Add New Contact",
+    color: "#00d1b2", // red, blue.... 
+    widthProsentage: 60, // The with of the popou measured by browser width 
+    animationDuration: .5, // in seconds, 0 = no animation 
+    showButtons: true, // You can hide this in case you want to use custom buttons 
+    confirmBtnContent: "Save", // The text on your confirm button 
+    cancleBtnContent: "Cancel", // the text on your cancel button 
+    confirmBtnClass: "button is-primary", // your class for styling the confirm button 
+    cancleBtnClass: "button is-light", // you class for styling the cancel button 
+    animation: "fadeInDown" // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown' 
+};
+  this.popup1.show(this.popup1.options);
   }
 
 }
--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { Popup } from 'ng2-opd-popup';
+import { Component } from '@angular/core';
 import { trigger,
   state,
   style,
@@ -13,22 +12,4 @@ import { trigger,
   styleUrls: ['./sandbox.component.css'],
 })
 export class SandboxComponent {
-
-  @ViewChild('popup1') popup1: Popup;
-  clickButton(){ 
-    this.popup1.options = {
-    header: "Your custom header",
-    color: "#5cb85c", // red, blue.... 
-    widthProsentage: 40, // The with of the popou measured by browser width 
-    animationDuration: 1, // in seconds, 0 = no animation 
-    showButtons: true, // You can hide this in case you want to use custom buttons 
-    confirmBtnContent: "OK", // The text on your confirm button 
-    cancleBtnContent: "Cancel", // the text on your cancel button 
-    confirmBtnClass: "btn btn-default", // your class for styling the confirm button 
-    cancleBtnClass: "btn btn-default", // you class for styling the cancel button 
-    animation: "fadeInDown" // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown' 
-};
-
-  this.popup1.show(this.popup1.options);
-  }
 }
